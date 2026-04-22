7bf812342c63fb2d192572d012ff9df41fa9d1eb
Osvin <ogamezm@miumg.edu.gt>
2026-04-22 15:45:10 -0600

    Ejercicio 1

---
+++ src/EjerciciosPolimorfismo/Main.java
@@ -0,0 +1,15 @@
+package EjerciciosPolimorfismo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Ejercicio 1
+		System.out.println("      Ejercicio 1. Animal y Perro\n");
+        Animal perro = new Perro();
+        perro.hacerSonido();
+        
+
+	}
+
+}
---
+++ src/EjerciciosPolimorfismo/Perro.java
@@ -0,0 +1,9 @@
+package EjerciciosPolimorfismo;
+
+public class Perro extends Animal {
+	
+    @Override
+    public void hacerSonido() {
+        System.out.println("Perro: Guau, guau");
+    }
+}
